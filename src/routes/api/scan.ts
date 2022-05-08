db1bff699247a38bc0a6653faa24683c2ec2a174
Eric Pedley <ericpedley@gmail.com>
2022-05-08 04:45:10 -0700

    more logging

--- src/routes/api/scan.ts
+++ src/routes/api/scan.ts
@@ -2,6 +2,9 @@ import {os} from '$lib/openscreen'
 import cookie from 'cookie'
 export async function get({url, request}) {
     const cookieObject = cookie.parse(request.headers.get('Cookie'))
+    console.log(cookieObject)
+    console.log(cookieObject.user)
+    console.log(typeof cookieObject.user)
     const user = cookieObject.user&&JSON.parse(cookieObject.user)
     const scanId = url.searchParams.get('scanId')
     const scan = await os.scan(scanId).get();
