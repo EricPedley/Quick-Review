03a5684576b4d3cf997ac99aa3aee7c853022b5a
Eric Pedley <ericpedley@gmail.com>
2022-05-08 04:53:09 -0700

    real solution to cookie problem

--- src/routes/api/scan.ts
+++ src/routes/api/scan.ts
@@ -1,8 +1,8 @@
 import {os} from '$lib/openscreen'
 import cookie from 'cookie'
 export async function get({url, request}) {
-    const cookieObject = request.headers.get('Cookie')&&cookie.parse(request.headers.get('Cookie')):{}
-    const user = cookieObject&&JSON.parse(cookieObject.user)
+    const cookieObject = cookie.parse(request.headers.get('Cookie')||'')
+    const user = JSON.parse(cookieObject?.user||'')
     const scanId = url.searchParams.get('scanId')
     const scan = await os.scan(scanId).get();
     return {
