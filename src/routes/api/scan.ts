952b4db18f19e9af0c7c6f2bee7c1e2d29f6a434
Eric Pedley <ericpedley@gmail.com>
2022-05-08 04:51:26 -0700

    made cookie parse depend on cookie being present in headers

--- src/routes/api/scan.ts
+++ src/routes/api/scan.ts
@@ -1,9 +1,8 @@
 import {os} from '$lib/openscreen'
 import cookie from 'cookie'
 export async function get({url, request}) {
-    console.log(request.headers.get('Cookie'))
-    const cookieObject = {}//cookie.parse(request.headers.get('Cookie'))
-    const user = cookieObject.user&&JSON.parse(cookieObject.user)
+    const cookieObject = request.headers.get('Cookie')&&cookie.parse(request.headers.get('Cookie')):{}
+    const user = cookieObject&&JSON.parse(cookieObject.user)
     const scanId = url.searchParams.get('scanId')
     const scan = await os.scan(scanId).get();
     return {
