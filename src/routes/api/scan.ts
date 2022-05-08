67943b42fed750cd8e527a45857ea81dd200d448
Eric Pedley <ericpedley@gmail.com>
2022-05-08 04:48:29 -0700

    stopped using cookie

--- src/routes/api/scan.ts
+++ src/routes/api/scan.ts
@@ -1,10 +1,8 @@
 import {os} from '$lib/openscreen'
 import cookie from 'cookie'
 export async function get({url, request}) {
-    const cookieObject = cookie.parse(request.headers.get('Cookie'))
-    console.log(cookieObject)
-    console.log(cookieObject.user)
-    console.log(typeof cookieObject.user)
+    console.log(request.headers.get('Cookie'))
+    const cookieObject = {}//cookie.parse(request.headers.get('Cookie'))
     const user = cookieObject.user&&JSON.parse(cookieObject.user)
     const scanId = url.searchParams.get('scanId')
     const scan = await os.scan(scanId).get();
